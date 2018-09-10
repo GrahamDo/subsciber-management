de4d70222cca01ecf3f7eaecdc0ff6e7cf15ff35
GrahamDo <graham@communicate.za.net>
2018-09-10 15:35:28 +0200

    Added unique key to field table for subscriber_id and title

--- create_db_script.sql
+++ create_db_script.sql
@@ -19,6 +19,7 @@ CREATE TABLE `field` (
   `type` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `ix_subscriber_id_title` (`subscriber_id`,`title`),
   KEY `fk_subscriber_idx` (`subscriber_id`),
   CONSTRAINT `fk_subscriber` FOREIGN KEY (`subscriber_id`) 
 	REFERENCES `subscriber` (`id`) ON DELETE CASCADE
