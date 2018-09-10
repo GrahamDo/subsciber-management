c6fd0f9746f443e08797303188d95f1ab04e786d
GrahamDo <graham@communicate.za.net>
2018-09-10 15:52:40 +0200

    Implemented field.list_by_subscriber_id

---
+++ field/list_by_subscriber_id.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/database.php';
+include_once '../objects/field.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$field = new Field($db);
+$results = $field->listBySubscriberId($_GET["subscriber_id"]);
+
+$arr = array();
+$arr["records"] = array();
+
+// fetch() is more performant than fetchAll():
+// http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+// GD: I'm not sure exactly what this means, or if it's actually valid in the
+//     version of PHP I'm using, but I found this in a tutorial.
+while ($row = $results->fetch(PDO::FETCH_ASSOC)) {   
+    extract($row);
+    
+    $item = array(
+        "id" => $id,
+        "title" => $title,
+        "type" => $type,
+        "type_text" => $field->getTypeText($type)
+    );
+    array_push($arr["records"], $item);
+}
+
+echo json_encode($arr);
--- objects/field.php
+++ objects/field.php
@@ -12,4 +12,28 @@ class Field
     {
         $this->conn = $db;
     }
+    
+    public function listBySubscriberId($subscriber_id)
+    {
+        $query = "SELECT " .
+                 "  id, " .
+                 "  title, " .
+                 "  type " .
+                 "FROM field " .
+                 "WHERE subscriber_id = " . $subscriber_id . ";";
+                 
+        $statement = $this->conn->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+    
+    public function getTypeText($type)
+    {
+        switch ($type) {
+            case 0: return "date";
+            case 1: return "number";
+            case 2: return "string";
+            case 3: return "boolean";
+        }
+    }
 }
